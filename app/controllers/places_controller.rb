5830153ad8e74c18e5c7c218407111cccb2d93a3
Donovan Palmer <donovanpalmer061@gmail.com>
2026-02-22 15:15:25 -0600

    Create places show action and view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,4 +6,12 @@ class PlacesController < ApplicationController
     # render places/index view
   end
 
+  def show
+    # find a Place
+    @place = Place.find_by({ "id" => params["id"] })
+    # find Entries for the Place
+    @entries = Entry.where({ "place_id" => @place["id"] })
+    # render places/show view with details about Place
+  end
+
 end
