304371c3280f4c7f857727d27e58012757a52805
Donovan Palmer <donovanpalmer061@gmail.com>
2026-02-22 15:17:00 -0600

    Create entries new/create actions and view

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,27 @@
+class EntriesController < ApplicationController
+
+  def new
+    @place = Place.find_by({ "id" => params["place_id"] })
+    # render entries/new view with new Entry form
+  end
+
+  def create
+    # start with a new Entry
+    @entry = Entry.new
+
+    # assign user-entered form data to Entry's columns
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+
+    # assign relationship between Entry and Place
+    @entry["place_id"] = params["place_id"]
+
+    # save Entry row
+    @entry.save
+
+    # redirect user
+    redirect_to "/places/#{@entry["place_id"]}"
+  end
+
+end
