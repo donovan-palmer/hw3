c5f097f0a7c361840550f19d494e5afea3bc84c8
Donovan Palmer <donovanpalmer061@gmail.com>
2026-02-22 15:14:14 -0600

    Configure routes for places and entries resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  # Root path route
+  get("/", { :controller => "places", :action => "index" })
+
+  # Places routes
+  get("/places", { :controller => "places", :action => "index" })
+  get("/places/new", { :controller => "places", :action => "new" })
+  get("/places/:id", { :controller => "places", :action => "show" })
+  post("/places", { :controller => "places", :action => "create" })
+
+  # Entries routes
+  get("/places/:place_id/entries/new", { :controller => "entries", :action => "new" })
+  post("/entries", { :controller => "entries", :action => "create" })
 end
